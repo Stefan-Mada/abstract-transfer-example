2f2dab663c8f299c10becc9281ae29e0a69d109a
Stefan <me@stefanmada.com>
2024-11-13 14:18:17 -0700

    Concretization function looks good

--- abstract-transfer.cpp
+++ abstract-transfer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "llvm/ADT/APInt.h"
 #include "llvm/Analysis/ValueTracking.h"
 #include "llvm/Support/KnownBits.h"
 #include <stdexcept>
@@ -53,11 +54,56 @@ ostream& operator<<(ostream& os, const KnownBits& dt)
     return os;
 }
 
+vector<APInt> concretization(const KnownBits& knownBits) {
+  vector<KnownBits> retVec;
+
+  const size_t bitwidth = knownBits.getBitWidth();
+  retVec.push_back(knownBits);
+
+  vector<KnownBits> tempVec;
+  for(size_t i = 0; i < bitwidth; ++i) {
+    for(auto knownBit : retVec) {
+      if(knownBit.One[i] && knownBit.Zero[i])
+        throw invalid_argument("How did we get to this impossibility?");
+      else if(knownBit.One[i] || knownBit.Zero[i])
+        tempVec.push_back(knownBit);
+      else {
+        knownBit.One.setBit(i);
+        tempVec.push_back(knownBit);
+        // then get the case where it's a known 0
+        knownBit.One.setBitVal(i, 0);
+        knownBit.Zero.setBit(i);
+        tempVec.push_back(knownBit);
+      }
+    }
+    retVec = tempVec;
+    tempVec.clear();
+  }
+
+  vector<APInt> realRetVec;
+  for(const auto& bit : retVec) {
+    realRetVec.push_back(bit.getConstant());
+  }
+
+  return realRetVec;
+}
+
 
 
 int main() {
-  auto allEnums = enumerateAllValues(3);
+  auto allEnums = enumerateAllValues(2);
   for(const auto& knownBit : allEnums)
-    cout << knownBit << " ";
-  cout << endl;
+    outs() << knownBit << " ";
+  outs() << "\n";
+
+  for(const auto& knownBit : allEnums) {
+    outs() << knownBit << ": ";
+    for(auto bitSet : concretization(knownBit)) {
+      bitSet.print(outs(), false);
+      outs() << ", ";
+    }
+    outs() << "\n";
+  }
+      
+  outs() << "\n";
 }
